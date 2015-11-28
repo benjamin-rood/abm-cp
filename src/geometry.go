68d1fe7d2f940f8e985543a7f83fcd11f5c68c70
benjamin-rood <bisr@icloud.com>
2015-11-28 16:45:12 +1300

    Created vector arithmetic functions. Implemented with support for n-dimension vectors. Using a slice to store vector co-ordinates means that all the functions (except crss-product), suit 2D, 3D, and higher dimensional Vectors.

--- src/geometry.go
+++ src/geometry.go
@@ -1,6 +1,79 @@
 package main
 
-import "math"
+import (
+	"errors"
+	"math"
+)
+
+// VecAddition v1 + v2 = v3
+func VecAddition(v Vector, u Vector) (Vector, error) {
+	if len(v) != len(u) {
+		return nil, errors.New("vector dimensions do not coincide")
+	}
+	var vPlusU Vector
+	for i := 0; i < len(v); i++ {
+		vPlusU = append(vPlusU, (v[i] + u[i])) //	add an element to the new vector which is the sum of element i from v1 and v2
+	}
+	return vPlusU, nil
+}
+
+// VecScalarMultiply scalar * v = [scalar*e1, scalar*e2, scalar*e3]
+func VecScalarMultiply(v Vector, scalar float32) (Vector, error) {
+	if len(v) == 0 {
+		return nil, errors.New("v is an empty vector")
+	}
+	var sm Vector
+	for i := range v {
+		sm = append(sm, (v[i] * scalar))
+	}
+	return sm, nil
+}
+
+// DotProduct returns the sum of the product of elements of two i-dimension vectors:
+// v₁u₁ + v₂u₂ + ⠂⠂⠂ + vᵢ₋₁+ vᵢ
+func DotProduct(v Vector, u Vector) (float64, error) {
+	if len(v) != len(u) {
+		return 0, errors.New("vector dimensions do not coincide")
+	}
+	var f float64
+	for i := 0; i < len(v); i++ {
+		f += float64((v[i] * u[i]))
+	}
+	return f, nil
+}
+
+// CrossProduct produces a new Vector orthogonal to both v and u.
+// Only supported for 3D vectors.
+func CrossProduct(v Vector, u Vector) (Vector, error) {
+	if len(v) != 3 || len(u) != 3 {
+		return nil, errors.New("vector dimension != 3")
+	}
+	var cp Vector
+	cp = append(cp, (v[y]*u[z] - v[z]*u[y]))
+	cp = append(cp, (v[z]*u[x] - v[x]*u[z]))
+	cp = append(cp, (v[x]*u[y] - v[y]*u[x]))
+	return cp, nil
+}
+
+// AngleFromOrigin calculates the angle of a given vector from the origin
+// relative to the x-axis of 𝐄 (the model environment)
+func AngleFromOrigin(v Vector) float64 {
+	return math.Atan2(float64(v[x]), float64(v[y]))
+}
+
+// RelativeAngle – does what it says on the box.
+func RelativeAngle(v Vector, u Vector) (float64, error) {
+	if len(v) == 0 || len(u) == 0 {
+		return 0, errors.New("v or u is an empty vector")
+	}
+	det := float64((v[x] * u[y]) - (v[y] * u[x]))
+	dot, err := DotProduct(v, u)
+	if err != nil {
+		return 0, err
+	}
+	angle := math.Atan2(det, dot)
+	return angle, nil
+}
 
 // UnitAngle will map any floating-point value to its angle on a unit circle.
 func UnitAngle(angle float64) float64 {
@@ -8,25 +81,37 @@ func UnitAngle(angle float64) float64 {
 	return angle - (twoPi * math.Floor(angle/twoPi))
 }
 
-// TranslatePositionToSector : translates the co-ordinates of a 2D vector to sector indices location
-func TranslatePositionToSector(ed float32, n int, vec Vec2f) (int, int) {
+// TranslatePositionToSector2D : translates the co-ordinates of a 2D vector to sector indices location (2D Version)
+func TranslatePositionToSector2D(ed float32, n int, v Vector) (int, int) {
 	fn := float32(n)
-	x := int((vec.x + ed) / (2 * ed) * fn)
-	y := int((vec.y + ed) / (2 * ed) * fn)
+	x := int((v[x] + ed) / (2 * ed) * fn)
+	y := int((v[y] + ed) / (2 * ed) * fn)
 	return x, y
 }
 
-// Vec2fMagniftude does the classic calculation
-func Vec2fMagniftude(v Vec2f) float64 {
-	return math.Sqrt(float64(v.x*v.x + v.y*v.y))
+// Magnitude does the classic calculation
+func Magnitude(v Vector) (float64, error) {
+	if len(v) == 0 {
+		return 0, errors.New("v is an empty vector")
+	}
+	var ǁvǁsq float64
+	for i := 0; i < len(v); i++ {
+		ǁvǁsq += float64(v[i] * v[i])
+	}
+	return math.Sqrt(ǁvǁsq), nil
 }
 
-// Vec2fDistance calculates the distance between two positions
-func Vec2fDistance(v1 Vec2f, v2 Vec2f) float64 {
-	xDiff := (v1.x - v2.x)
-	yDiff := (v1.y - v2.y)
-	vd := Vec2f{xDiff, yDiff}
-	return Vec2fMagniftude(vd)
+// VectorDistance calculates the distance between two positions
+func VectorDistance(v Vector, u Vector) (float64, error) {
+	if len(v) != len(u) {
+		return 0, errors.New("vector dimensions do not coincide")
+	}
+	vd := Vector{}
+	for i := 0; i < len(v); i++ {
+		diff := (v[i] - u[i])
+		vd = append(vd, diff)
+	}
+	return Magnitude(vd)
 }
 
 /*ColourDistance quantifies the value difference between two ColRGB structs,
--- src/model.go
+++ src/model.go
@@ -1,41 +1,62 @@
 package main
 
-/*ColRGB stores a standard 8-bit per channel Red Green Blue colour representation. */
+/*
+MTime holds the model's representation of the time metrics.
+Turn – The cycle length for all agents ∈ 𝐄 to perform 1 (and only 1) Action.
+Phase – Division of a Turn, between agent sets, environmental effects/factors,
+				and updates to populations and model conditions (via external).
+				One Phase is complete when all members of a set have performed an Action
+				or all requirements for the model's continuation have been fulfilled.
+Action – An individual 'step' in the model. All Actions have a cost:
+				the period (number of turns) before that specific Action can be
+				performed again. For most actions this is zero.
+				Some Actions could also *stop* any other behaviour by that agent
+				for a period.
+*/
+type MTime struct {
+	turn   int
+	phase  int
+	action int
+}
+
+const (
+	x = iota
+	y
+	z
+)
+
+/*
+ColRGB stores a standard 8-bit per channel Red Green Blue colour representation.
+*/
 type ColRGB struct {
 	red   byte
 	green byte
 	blue  byte
 }
 
-/*Environment specifies the boundary / dimensions of the working model. They extend in both positive and negative directions, oriented at the center. Setting any field (eg. zBounds) to zero will reduce the dimensionality of the model. For most cases, a 2D environment will be sufficient. */
+/*
+Environment specifies the boundary / dimensions of the working model. They
+extend in both positive and negative directions, oriented at the center. Setting
+any field (eg. zBounds) to zero will reduce the dimensionality of the model. For
+most cases, a 2D environment will be sufficient.
+*/
 type Environment struct {
 	xBounds float32
 	yBounds float32
 	zBounds float32
 }
 
-// Vec2f – 2D co-ordinates
-type Vec2f struct {
-	x float32
-	y float32
-}
+// Vector : Any sized dimension representation of an element of vector space.
+type Vector []float32
 
-// Vec2fCalc - 2D vector arithmetic
-type Vec2fCalc interface {
-	vecAddition(Vec2f, Vec2f) Vec2f
-	vecScalarMultiply(Vec2f, float32) Vec2f
-	dotProduct(Vec2f, Vec2f) float32
-	magnitude(Vec2f) float32
-	angleFromOrigin(Vec2f) float32
-	relativeAngle(Vec2f, Vec2f) float32
-}
+// VectorCalc - Vector arithmetic, in (len(vector))dimension (i.e. any) space!
 
 type colourPolymorhicPrey struct {
-	pos       Vec2f
+	pos       Vector
 	movS      float32
 	movA      float32
 	heading   float32
-	direction Vec2f
+	direction Vector
 	lifetime  int32
 	hunger    int32
 	fertility int32 //	interval measurement between birth and sex
@@ -44,15 +65,15 @@ type colourPolymorhicPrey struct {
 }
 
 type visualPredator struct {
-	pos       Vec2f
+	pos       Vector
 	movS      float32
 	movA      float32
-	heading   float32
-	direction Vec2f
-	lifetime  int32
-	hunger    int32
-	fertility int32 //	interval measurement between birth and sex
-	gravid    bool  //	i.e. pregnant
+	heading   float32 //	angle of direction relative to origin ∈ 𝐄
+	direction Vector  //	unit vector for
+	lifetime  int32   //	counter for number of turns agent exists in the model
+	hunger    int32   //	measurement reflecting
+	fertility int32   //	interval measurement between birth and sex
+	gravid    bool    //	i.e. pregnant
 	visRange  float32
 	visAcuity float32
 	imprint   ColRGB
