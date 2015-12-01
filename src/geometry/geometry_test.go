1077cc2e4838188ced372301e425f3542f4fb8b8
benjamin-rood <bisr@icloud.com>
2015-12-01 21:32:40 +1300

    added CrossProduct test in geometry package

--- src/geometry/geometry_test.go
+++ src/geometry/geometry_test.go
@@ -8,13 +8,17 @@ import (
 
 func Test(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
-	xval := float64(-0.5)
-	yval := float64(0.5)
-	wval := float64(1.1)
-	zval := float64(-9.9)
+	uval := float64(rand.Int())
+	vval := float64(rand.Int())
+	wval := float64(rand.Int())
+	xval := float64(rand.Int())
+	yval := float64(rand.Int())
+	zval := float64(rand.Int())
+	randU := rand.Float64()
+	randV := rand.Float64()
+	randW := rand.Float64()
 	randX := rand.Float64()
 	randY := rand.Float64()
-	randW := rand.Float64()
 	randZ := rand.Float64()
 
 	var vecAdditionTests = []struct {
@@ -65,5 +69,25 @@ func Test(t *testing.T) {
 			t.Errorf("DotProduct(%v, %v) == %v, want %v\n", e.v, e.u, got, e.want)
 		}
 	}
+	/*
+	  y*w - z*v
+	  z*u - x*w
+	  x*v - y*u
+	*/
+	var crossProductTests = []struct {
+		v, u, want Vector
+	}{
+		{Vector{1.0, 2.0, 3.0}, Vector{3.0, 4.0, 5.0}, Vector{-2.0, 4.0, -2.0}},
+		{Vector{xval, yval, zval}, Vector{uval, vval, wval},
+			Vector{(yval*wval - zval*vval), (zval*uval - xval*wval), (xval*vval - yval*uval)}},
+		{Vector{randX, randY, randZ}, Vector{randU, randV, randW},
+			Vector{(randY*randW - randZ*randV), (randZ*randU - randX*randW), (randX*randV - randY*randU)}},
+	}
 
+	for _, f := range crossProductTests {
+		got, _ := CrossProduct(f.v, f.u)
+		if got.Equal(f.want) == false {
+			t.Errorf("VecScalarMultiply(%v, %v) == %v, want %q\n", f.v, f.u, got, f.want)
+		}
+	}
 }
